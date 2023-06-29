3169723b9e3716a5a5a2e7e1c5f6630a400523a4
Deivis Rodriguez <deivisjoro@yahoo.com>
2023-06-29 08:23:52 -0500

    validacion form

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -12,6 +12,9 @@
         error.value = false;
         if(description.value.trim().length===0){
             error.value = true;
+            setTimeout(()=>{
+                error.value = false;
+            }, 2000)
             return;
         }
 
@@ -24,14 +27,14 @@
 
 <template>
     <div>
-        <form @submit.prevent="addTask" class="flex mb-2 py-4 items-center">
+        <form @submit.prevent="addTask" class="flex mb-2 mt-2 py-1 items-center">
             <input type="text" v-model="description" class="rounded-lg w-full h-10 mr-2 text-blue-600 border-blue-300 border-2 focus:border-blue-900 outline-none p-2 text-lg" placeholder="descripcion tarea, ejemplo: finalizar proyecto..." name="description">
             <button class="bg-blue-500 hover:bg-blue-800 outline-none text-white font-bold rounded-lg px-4 py-2">
                 Agregar
             </button>
         </form>
     </div>
-    <div v-if="error">
+    <div v-if="error" class="flex items-center justify-center bg-red-200 p-2 mb-3 rounded-full text-sm font-bold text-red-700">
         Debe ingresar una descripcion de la tarea
     </div>
 </template>
